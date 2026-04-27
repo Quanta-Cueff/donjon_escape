7fe648eca16c78d29ec15270126c179cee714c98
cueffquanta <t.bouin@rns.ecolescreatives.com>
2026-04-27 13:02:44 +0200

    v6

--- Assets/power_texte.cs
+++ Assets/power_texte.cs
@@ -6,20 +6,28 @@ public class power_texte : MonoBehaviour
 {
     public TextMeshPro textMeshPro;
     public float time;
-    public void powertext(Text power)
+    public Transform position;
+    public TextMeshPro TextMeshPro;
+    public void Start()
     {
-        transform.position *= 0;
+        transform.position = position.position;
+        transform.localScale *= 0;
+    }
+    public void powertext(string power)
+    {
+        transform.position = position.position;
         transform.localScale *= 0;
         time = 0;
-        textMeshPro.text = power.text;
+        textMeshPro.text = power;
     }
     private void Update()
     {
-        if (time > 1)
+        if (time < 1)
         { 
-            transform.position = new Vector3(transform.position.x, transform.position.y+(Time.deltaTime),0);
-            transform.localScale = new Vector3(1 / time, 1 / time, 1);
-            time += Time.deltaTime;
+            transform.position = new Vector3(0,(time*2),0) + position.position;
+            transform.localScale = new Vector3(Mathf.Sqrt(time), Mathf.Sqrt(time), 1);
+            time += Time.deltaTime/3;
+            textMeshPro.color = new Color(1, 1, 1, (1 / time) - 1);
         }
     }
 }
--- Assets/script/power_up.cs
+++ Assets/script/power_up.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
+
 public class power_up : MonoBehaviour
 {
     public listo_of_power listo_Of_Power;
     public mana_manageur mena;
+    public power_texte power_Texte;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
@@ -18,30 +20,39 @@ public class power_up : MonoBehaviour
                 print("tp");
                 power_grow(1,1,1,collision.gameObject);
                 liste.TP = listo_Of_Power.TP;
+                power_Texte.powertext("téléportation");
             }
             if (!liste.Sh & listo_Of_Power.Sh)
             {
                 print("shilde");
                 power_grow(2, 1, 1, collision.gameObject);
                 liste.Sh = listo_Of_Power.Sh;
+                power_Texte.powertext("bouclier");
+
             }
             if (!liste.WJ & listo_Of_Power.WJ)
             {
                 print("wole jumpe");
                 power_grow(1, 1, 1, collision.gameObject);
                 liste.WJ = listo_Of_Power.WJ;
+                power_Texte.powertext("wole jumpe");
+
             }
             if (!liste.Sw & listo_Of_Power.Sw)
             {
                 print("sword");
                 power_grow(1, 1, 1, collision.gameObject);
                 liste.Sw = listo_Of_Power.Sw;
+                power_Texte.powertext("épée de mana");
+
             }
             if (!liste.MR & listo_Of_Power.MR)
             {
                 print("mana regen");
                 power_grow(1, 2, 1, collision.gameObject);
                 liste.MR = listo_Of_Power.MR;
+                power_Texte.powertext("régénération de mana");
+
             }
             Destroy(gameObject);
         }
