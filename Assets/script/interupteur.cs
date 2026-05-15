4e0d8402f24aff65467abf098eecba09c0102842
Quanta-Cueff <t.bouin@rns.ecolescreatives.com>
2026-05-15 18:01:33 +0200

    v23

--- Assets/script/interupteur.cs
+++ Assets/script/interupteur.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class interupteur : MonoBehaviour
 {
     public pilone_manageur manageur;
     public bool nf;
     public bool ison;
-    
+    public Tilemap tile;
+    private float coldawne;
+    private bool flip_flop;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,10 +18,21 @@ public class interupteur : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(nf & Input.GetMouseButtonDown(1))
+        if(coldawne > 0)
         {
+            coldawne -= Time.deltaTime;
+            flip_flop =true;
+        }else if(flip_flop)
+        {
+            tile.color *= 2f;
+            flip_flop = false;
+        }
+        if (nf & Input.GetMouseButtonDown(1))
+        {
+            tile.color *= 0.5f;
             manageur.nf = !manageur.nf;
             ison =! ison;
+            coldawne = 0.5f;
         }
         nf = false;
     }
